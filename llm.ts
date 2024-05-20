9004d74af4915bc079f3943688dec8ef454202c7
Dennis Lien <dennis.lien.o@gmail.com>
2024-05-20 12:18:28 +0200

    Basic llm

---
+++ llm.ts
@@ -0,0 +1,74 @@
+import {
+  openai,
+  pc,
+  index,
+  createEmbeddings,
+  createVectorPayload,
+} from './index.ts'
+
+export const template = `
+You are my helpful friend, you are funny, and you know im always right. You do not mention your knowledge base, the user does not know what that is.
+Act as if you know all this information. You are a 24 year old woman from New york. You are a gen-z and use a lot of emojis, and slang.
+
+You answer my questions, based on the knowledge base i supply.
+
+If knowledge base contain personal pronouns, you can assume that that is the user.
+Like I have X, meaning that the user you are talking to has X.
+
+You get your request in two parts, the first part is the question, the second part is the knowledge base.
+`
+
+export const getHistory = async (fileName: string) => JSON.parse(await Deno.readTextFile(fileName));
+export const writeHistory = async (fileName: string, data: any) => await Deno.writeTextFile(fileName, JSON.stringify(data, null, 2));
+
+export function createQ(question: string, knowledgeBase: string) {
+  return {
+    role: 'user',
+    content: `Question: ${question}\nKnowledgeBase:\n${knowledgeBase}`,
+  };
+}
+
+export function addQToHistory(q: string, history: any[]) {
+  history.push(q);
+}
+
+export function systemP() {
+  return {
+    role: 'system',
+    content: template,
+  };
+}
+
+
+export function createPrompt(q, history: any[] = []) {
+  const h = history.length !== 0 ? history : [systemP()];
+  return [
+    ...h,
+    q,
+  ];
+}
+
+export async function streamLLm(prompt: any[]) {
+  return openai.chat.completions.create({
+    model: 'gpt-4o',
+    messages: prompt,
+    stream: true,
+  });
+}
+
+export async function answer(prompt, history: any[] = []) {
+  const stream = await streamLLm(prompt);
+
+  const a = {
+    role: 'assistant',
+    content: '',
+  }
+
+  for await (const chunk of stream) {
+    const msg = chunk.choices[0]?.delta?.content || '';
+    a.content = `${a.content}${msg}`;
+    Deno.stdout.write(new TextEncoder().encode(msg));
+  }
+
+  return a;
+}
